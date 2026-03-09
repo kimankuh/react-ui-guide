8be7db1ffe6c8df070738bb58709fd9c1dd456fa
mankuh <kimankuh@naver.com>
2026-03-09 10:31:25 +0900

    hash라우터로 변경

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
 
 import App from './App.jsx'
 import './index.css'
@@ -8,9 +9,9 @@ import './index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* react router 기능을 전체 앱에 적용. BrowserRouter => 페이지 이동 기능을 제공 */}
-    <BrowserRouter>
+    <HashRouter>
         {/* 앱 전체 */}
         <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
