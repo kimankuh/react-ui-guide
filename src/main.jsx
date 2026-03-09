d7b49f2b7055dd6e2ee08d8f0a43bfe641e1d5ae
mankuh <kimankuh@naver.com>
2026-03-09 10:45:57 +0900

    브라우저 라우터로 변경 및 속성 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
-// import { BrowserRouter } from 'react-router-dom'
+// import { HashRouter } from 'react-router-dom' // Github pages 사용시 => router에 '/' 경로를 사용하려면. vite.config.js 파일엔 /react-ui-guide/ 이렇게 설정되어있어 '/'를 사용할 수 없기때문
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './App.jsx'
 import './index.css'
@@ -9,9 +9,10 @@ import './index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* react router 기능을 전체 앱에 적용. BrowserRouter => 페이지 이동 기능을 제공 */}
-    <HashRouter>
+    {/* Github pages 사용시 'HashRouter'를 사용하지 않고 'BrowserRouter'를 유지하려면 basename="/react-ui-guide" 추가 */}
+    <BrowserRouter basename="/react-ui-guide">
         {/* 앱 전체 */}
         <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
