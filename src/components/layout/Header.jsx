c257f6bcbbb8c92223f2540a4ed71b6e293eac2b
mankuh <kimankuh@naver.com>
2026-03-09 11:02:17 +0900

    수정된거야?

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -15,7 +15,7 @@ const Header = ({}) => {
     ]
     return (
         <div className={styles.header}>{/* css module 사용시 대시가 포함된 경우 대괄호+따옴표(styles["ui-header"])로 묶기. js 내부에 작성된 문자열이니까. */}
-            <h1 className={styles.h1}>React Guide</h1>
+            <h1 className={styles.h1}>React Guide..</h1>
 
             <nav>
                 <ul className={styles.sidebar}>
